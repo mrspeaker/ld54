84cebb1e98541d2fcadf03e5cf27ffd267752b81
Mr Speaker <mrspeaker@gmail.com>
2023-09-30 21:39:25 +0800

    More invisible tilemaps

--- src/game.rs
+++ src/game.rs
@@ -83,17 +83,33 @@ fn game_setup(
 
     let texture_handle2: Handle<Image> = asset_server.load("img/tiles.png");
 
+    commands.spawn((
+        SpriteBundle {
+            texture: texture_handle2.clone(),
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0)
+                .with_scale(Vec3::new(1.8, 1.62, 0.0)),
+            ..default()
+        },
+        OnGameScreen,
+    ));
+
     let map_size = TilemapSize { x: 32, y: 32 };
     let tilemap_entity = commands.spawn_empty().id();
     let mut tile_storage = TileStorage::empty(map_size);
 
-    fill_tilemap(
-        TileTextureIndex(1),
-        map_size,
-        TilemapId(tilemap_entity),
-        &mut commands,
-        &mut tile_storage,
-    );
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+            let tile_entity = commands
+                .spawn(TileBundle {
+                    position: tile_pos,
+                    tilemap_id: TilemapId(tilemap_entity),
+                    ..Default::default()
+                })
+                .id();
+            tile_storage.set(&tile_pos, tile_entity);
+        }
+    }
 
     let tile_size = TilemapTileSize { x: 16.0, y: 16.0 };
     let grid_size = tile_size.into();
